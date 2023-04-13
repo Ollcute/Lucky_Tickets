7fdd5ef7c8e4f34bf8ae0be8510f32dc18161e5e
Ollcute <112689413+Ollcute@users.noreply.github.com>
2023-04-13 12:22:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a six-digit ticket number: ");
+        string ticketNumber = Console.ReadLine();
+
+        if (ticketNumber.Length != 6)
+        {
+            Console.WriteLine("Invalid ticket number. Please enter exactly six digits.");
+            return;
+        }
+
+        bool isLucky = IsTicketLucky(ticketNumber);
+
+        if (isLucky)
+        {
+            Console.WriteLine("Congratulations! The ticket is lucky.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, the ticket is not lucky.");
+        }
+    }
+
+    static bool IsTicketLucky(string ticketNumber)
+    {
+        int sumFirstThreeDigits = 0;
+        int sumLastThreeDigits = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            int digit = int.Parse(ticketNumber[i].ToString());
+            sumFirstThreeDigits += digit;
+        }
+
+        for (int i = 3; i < 6; i++)
+        {
+            int digit = int.Parse(ticketNumber[i].ToString());
+            sumLastThreeDigits += digit;
+        }
+
+        return sumFirstThreeDigits == sumLastThreeDigits;
+    }
+}
